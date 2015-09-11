606f0d9cf629ad3e9f8ae870d1812e4d96386c1c
Vinh Nguyen <vnguyen.cheme@gmail.com>
2015-09-11 10:40:16 -0600

    added post route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var watson = require('watson-developer-cloud');
+var i18n = require('i18next');
+var extend = require('util')._extend
+ 
+var personality_insights = watson.personality_insights({
+  username: '81945be6-cd8d-4dbf-9954-2514d254e786',
+  password: 'nj8bLN9zryeQ',
+  version: 'v2'
+});
+ 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next) {
+  var parameters = extend(req.body, { acceptLanguage : i18n.lng() });
+  personality_insights.profile(parameters,
+    function (err, response) {
+      if (err)
+        console.log('error:', err);
+      else
+        res.json(response);
+
+  });
+});
 module.exports = router;
